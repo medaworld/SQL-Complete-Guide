024f2814cb8ddcd667aca431dd3a73b14f67230d
MEDA <118144136+medaworld@users.noreply.github.com>
2023-06-19 23:08:18 -0700

    Create generated columns

--- data-definition/18-generated-columns.sql
+++ data-definition/18-generated-columns.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
     last_name VARCHAR(200) NOT NULL,
     full_name VARCHAR(401) GENERATED ALWAYS AS (CONCAT(first_name,' ', last_name)),
     -- full_name VARCHAR(300) NOT NULL,
-    -- yearly_salary INT CHECK (yearly_salary > 0),
+    yearly_salary INT CHECK (yearly_salary > 0),
     current_status ENUM('self-employed', 'employed', 'unemployed') -- MYSQL
     -- current_status employment_status
 );
@@ -33,3 +33,6 @@ CREATE TABLE conversations (
     message TEXT NOT NULL,
     date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users (first_name, last_name, yearly_salary, current_status)
+VALUES ('Max', 'Schwarz', 19000, 'self-employed')
