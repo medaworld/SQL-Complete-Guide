de5131ce51985cda6ec786319a907b10e7d961ef
MEDA <118144136+medaworld@users.noreply.github.com>
2023-07-02 20:26:20 -0700

    INNER JOIN vs LEFT JOIN examples

---
+++ data-normalization/08-more-joins.sql
@@ -0,0 +1,4 @@
+SELECT name AS city_name, u.first_name, u.last_name, a.street, a.house_number
+FROM cities AS c
+LEFT JOIN addresses AS a ON c.id = a.city_id
+LEFT JOIN users AS u ON u.address_id = a.id;
