8c48d46cb2364ffb05cafa4cc0d7d4da4603d729
MEDA <118144136+medaworld@users.noreply.github.com>
2023-07-02 19:23:31 -0700

    Use RIGHT JOIN

--- data-normalization/06-left-join.sql
+++ data-normalization/06-left-join.sql
@@ -1,4 +1,4 @@
 SELECT *
-FROM addresses AS a
-LEFT JOIN users AS u ON a.id = u.address_id
+FROM users AS u
+RIGHT JOIN addresses AS a ON a.id = u.address_id
 LEFT JOIN cities AS c ON c.id = a.city_id;
