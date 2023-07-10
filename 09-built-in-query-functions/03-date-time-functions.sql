08a799bbcba2d127209b8eee2363557b08525176
MEDA <118144136+medaworld@users.noreply.github.com>
2023-07-09 23:34:47 -0700

    Work with weekdays

--- 09-built-in-query-functions/03-date-time-functions.sql
+++ 09-built-in-query-functions/03-date-time-functions.sql
@@ -1,2 +1,13 @@
-SELECT EXTRACT(MINUTE FROM last_checkin), last_checkin
+-- DOW postgreSQL only
+-- SELECT EXTRACT(DOW FROM last_checkin), last_checkin
+-- FROM memberships;
+
+-- SELECT WEEKDAY(last_checkin) + 1, last_checkin
+-- FROM memberships;
+
+--postgreSQL only
+-- SELECT CONVERT(last_checkin, DATE), CONVERT(last_checkin, TIME)
+-- FROM memberships;
+
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
 FROM memberships;
