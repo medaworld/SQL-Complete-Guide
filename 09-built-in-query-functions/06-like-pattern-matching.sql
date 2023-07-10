bf7f65360775c03fdf1f1ac26d8b8f01ecc4d639
MEDA <118144136+medaworld@users.noreply.github.com>
2023-07-10 16:03:55 -0700

    Understand LIKE & Pattern matching

---
+++ 09-built-in-query-functions/06-like-pattern-matching.sql
@@ -0,0 +1,13 @@
+-- SELECT first_name LIKE '_o%', first_name
+-- FROM memberships;
+
+-- SELECT first_name
+-- FROM memberships
+-- WHERE first_name LIKE 'J____';
+
+-- SELECT first_name LIKE 'ma%', first_name
+-- FROM memberships;
+
+SELECT first_name
+FROM memberships
+WHERE first_name LIKE 'J____';
