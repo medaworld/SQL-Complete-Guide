81e1b8c908ac538a29aea77dfb70595629e1e026
MEDA <118144136+medaworld@users.noreply.github.com>
2023-07-09 20:59:30 -0700

    Understand string functions with SELECT

---
+++ 09-built-in-query-functions/02-string-functions.sql
@@ -0,0 +1,9 @@
+-- SELECT CONCAT(first_name, ' ',last_name)
+-- FROM memberships;
+
+-- postgreSQL only
+-- SELECT first_name || ' ' || last_name
+-- FROM memberships;
+
+SELECT CONCAT('$ ', price)
+FROM memberships;
