a842806d5c1305636866e1b6cdf0295416a3c192
MEDA <118144136+medaworld@users.noreply.github.com>
2023-07-10 23:53:02 -0700

    Combining multiple joins

--- 13-complete-example/09-queries-with-relations.sql
+++ 13-complete-example/09-queries-with-relations.sql
@@ -1,3 +1,14 @@
+-- SELECT 
+--     e.id AS event_id,
+--     e.name,
+--     e.date_planned,
+--     loc.title, 
+--     loc.street, 
+--     loc.house_number, 
+--     loc.city_name 
+-- FROM events AS e
+-- INNER JOIN locations AS loc ON e.location_id = loc.id;
+
 SELECT 
     e.id AS event_id,
     e.name,
@@ -5,6 +16,10 @@ SELECT
     loc.title, 
     loc.street, 
     loc.house_number, 
-    loc.city_name 
+    loc.city_name,
+    u.first_name,
+    u.last_name
 FROM events AS e
-INNER JOIN locations AS loc ON e.location_id = loc.id;
+INNER JOIN locations AS loc ON e.location_id = loc.id
+INNER JOIN events_users AS eu ON eu.event_id = e.id
+INNER JOIN users AS u ON eu.user_id = u.id;
