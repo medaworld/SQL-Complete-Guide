4eb84ea8ac43c645afde31c91c367f3026991d91
MEDA <118144136+medaworld@users.noreply.github.com>
2023-07-11 00:21:48 -0700

    Use the HAVING clause

--- 13-complete-example/10-more-queries.sql
+++ 13-complete-example/10-more-queries.sql
@@ -3,6 +3,8 @@
 
 -- SELECT COUNT(id) from locations;
 
-SELECT c.name, loc.street, COUNT(loc.id) AS num_locations FROM cities AS c
+SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
 LEFT JOIN locations AS loc ON loc.city_name = c.name
-GROUP BY c.name, loc.street;
+-- WHERE loc.city_name = 'Munich'
+GROUP BY c.name
+HAVING COUNT(loc.id) > 1;
